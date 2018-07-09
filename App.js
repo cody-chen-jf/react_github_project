cf2cab3ebbf9098ea651c55c26a0232982f16bfb
cody-chen-jf <459741515@qq.com>
2018-07-09 20:50:13 +0800

    add tab bar

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, View, Image, Text } from 'react-native'
+import TabNavigator from 'react-native-tab-navigator'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,12 +17,68 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTab: 'home'
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_popular'}
+            title="最热"
+            renderIcon={() => <Image style={styles.image1} source={require('./res/images/ic_polular.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image1, {tintColor: 'blue'}]} source={require('./res/images/ic_polular.png')}/>}
+            badgeText="1"
+            onPress={() => this.setState({selectedTab: 'tb_popular'})}>
+            <View style={styles.page1}>
+              <Text>
+                aaa
+              </Text>
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_trending'}
+            title="趋势"
+            renderIcon={() => <Image style={styles.image1} source={require('./res/images/ic_polular.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image1, {tintColor: 'blue'}]} source={require('./res/images/ic_polular.png')}/>}
+            badgeText="1"
+            onPress={() => this.setState({selectedTab: 'tb_trending'})}>
+            <View style={styles.page1}>
+              <Text>
+                aaa
+              </Text>
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_favorite'}
+            title="收藏"
+            renderIcon={() => <Image style={styles.image2} source={require('./res/images/ic_trending.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image2, {tintColor: 'blue'}]} source={require('./res/images/ic_trending.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tb_favorite'})}>
+            <View style={styles.page2}>
+              <Text>
+                bbb
+              </Text>
+            </View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_my'}
+            title="我的"
+            renderIcon={() => <Image style={styles.image2} source={require('./res/images/ic_trending.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image2, {tintColor: 'blue'}]} source={require('./res/images/ic_trending.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tb_my'})}>
+            <View style={styles.page2}>
+              <Text>
+                bbb
+              </Text>
+            </View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     )
   }
@@ -30,18 +87,22 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  page1: {
+    flex: 1,
+    backgroundColor: 'yellow'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  page2: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  image1: {
+    height: 22,
+    width: 22
+  },
+  image2: {
+    height: 22,
+    width: 22
   }
 })
